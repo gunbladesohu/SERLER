195f03ac1020fcab678183a4e0cb23ccd73c41ac
Lukas Pirl <git@lukas-pirl.de>
2015-09-07 14:24:03 +1200

    added dummy tests to set up Travis CI

--- test/unit/evidence_source_test.rb
+++ test/unit/evidence_source_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class EvidenceSourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "dummy" do
+    # this test tests nothing and should be removed
+    assert true
+  end
+  test "dummy loading of fixtures" do
+    # this test tests framework functionality and should be removed
+    assert EvidenceSource.all.count == 2
+  end
 end
