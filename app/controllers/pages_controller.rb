61173c681ae63745d1739197e4166f63535d9f61
Lukas Pirl <git@lukas-pirl.de>
2015-09-22 13:27:52 +1200

    removed legacy "tags", made new dummy search result page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
   def index
-      # legacy - to display all the content in EvidenceItem
-    @item=EvidenceItem.all
-      # legacy end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,5 @@
 class SearchController < ApplicationController
   def index
-        # legacy - full text search, display the search result (in EvidenceItem) via app/views/search/index.html.erb
-    @search=EvidenceItem.search do
-      fulltext params[:q]
-    end
-    @evidence=@search.results
-    @q=:q
-        # legacy end
+    @evidence_items=EvidenceItem.all
   end
 end
