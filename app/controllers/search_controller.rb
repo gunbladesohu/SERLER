4fd08f95a696cd95b6db46531ab56e556a01daf3
Lukas Pirl <git@lukas-pirl.de>
2015-09-22 23:22:15 +1200

    made example search results a table

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,8 @@
 class SearchController < ApplicationController
 
-  AVAILABLE_COLUMNS = [
-    "benfit",
-    "evidence_source",
-  ]
-
-  ENABLED_COLUMNS_DEFAULT = [
-    "benefit",
-  ]
-
   def index
-
     @search_query = params[:query]
     @evidence_items = EvidenceItem.all
-
-    @columns_available = AVAILABLE_COLUMNS
-    @columns_enabled = if params[:columns].present?
-      params[:columns]
-    else
-      ENABLED_COLUMNS_DEFAULT
-    end
   end
 
 
