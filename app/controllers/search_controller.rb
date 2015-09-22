b7fd936c560eb4be21a811bc9342348accf215ea
Lukas Pirl <git@lukas-pirl.de>
2015-09-22 23:15:09 +1200

    UI mock and few mock code (will abandon this idea)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,26 @@
 class SearchController < ApplicationController
+
+  AVAILABLE_COLUMNS = [
+    "benfit",
+    "evidence_source",
+  ]
+
+  ENABLED_COLUMNS_DEFAULT = [
+    "benefit",
+  ]
+
   def index
-    @evidence_items=EvidenceItem.all
-    @search_query=params[:q]
+
+    @search_query = params[:query]
+    @evidence_items = EvidenceItem.all
+
+    @columns_available = AVAILABLE_COLUMNS
+    @columns_enabled = if params[:columns].present?
+      params[:columns]
+    else
+      ENABLED_COLUMNS_DEFAULT
+    end
   end
+
+
 end
