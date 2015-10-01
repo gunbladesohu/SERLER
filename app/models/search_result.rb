adcdf2e3e0c37da9eec7b92bd9274ad842b503df
Lukas Pirl <git@lukas-pirl.de>
2015-10-02 09:29:08 +1300

    improved form for SearchResult

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,4 +1,4 @@
 class SearchResult < ActiveRecord::Base
-  attr_accessible :comment, :name, :evidence_items
+  attr_accessible :comment, :name, :evidence_items, :evidence_item_ids
   has_and_belongs_to_many :evidence_items
 end
