3049caa02e7e8a7b1206083748c987e9828f3672
bakhshb <bakhshb@gmail.com>
2015-10-05 03:56:46 -0300

    testing the function of saving queries and results

--- features/step_definitions/save_steps.rb
+++ features/step_definitions/save_steps.rb
@@ -1,6 +1,15 @@
 Given(/^I click on "(.*?)" button under the title "(.*?)"/) do |save, title|
-  click_on(save)
   assert page.has_content?(title)
+
+  @saved_search_result = SavedSearchResult.new(:name => "test", :comment => "result")	
+  @saved_search_query = SavedSearchQuery.new(:name => "test", :comment => "query")
+  
+  if @saved_search_result.save
+  	click_on(save)
+  elsif @saved_search_query.save
+  	click_on(save)
+  end
+
 end
 
 Then(/^I should be able to save search query/) do
