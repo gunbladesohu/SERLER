dbbd465a91e663cea26e377429cf7f0b9b512504
Hao Zhang <klaushaozhang@gmail.com>
2015-10-07 00:49:36 +1300

    add search Method/methodology stuff.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,9 +21,9 @@ class SearchController < ApplicationController
   
   def index
     @search_params = params[:q]
-    @search = EvidenceItem.search(@search_params)
-    @search.build_condition
-		@search.build_sort if @search.sorts.empty?
+		@search_evidence_item = EvidenceItem.ransack(@search_params)
+    @search_evidence_item.build_condition
+		@search_evidence_item.build_sort if @search_evidence_item.sorts.empty?
 
     unless params.count > 2
       render 'search_form'
@@ -50,7 +50,7 @@ class SearchController < ApplicationController
       @saved_search_query = SavedSearchQuery.new
     end
 
-    @evidence_items = @search.result
+    @evidence_items = @search_evidence_item.result
     render 'results'
     return
   end
