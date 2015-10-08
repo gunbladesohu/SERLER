9535200529bd24b9af1cb8aa46c0c367ba1554a3
Hao Zhang <klaushaozhang@gmail.com>
2015-10-08 15:24:46 +1300

    delete unused code

--- app/models/evidence_item.rb
+++ app/models/evidence_item.rb
@@ -1,12 +1,10 @@
 class EvidenceItem < AbstractUserCreatedModel
-  attr_accessible :benefit, :result, :software_engineering_methodologies_attributes, :software_engineering_methods_attributes
+  attr_accessible :benefit, :result
   belongs_to :evidence_source
   has_and_belongs_to_many :software_engineering_methods
   has_and_belongs_to_many :software_engineering_methodologies
   has_and_belongs_to_many :saved_search_results
   
-  accepts_nested_attributes_for :software_engineering_methodologies, :software_engineering_methods
-
 	RANSACKABLE_ATTRIBUTES = ["benefit","result"]
 
   def self.ransackable_attributes auth_object = nil
