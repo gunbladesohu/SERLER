b7144e4a53fbc453d510e1482352af367ba4f7cc
Lukas Pirl <git@lukas-pirl.de>
2015-10-08 21:03:10 +1300

    search for Method(onlogy): made only id non-searchable

--- app/models/software_engineering_method.rb
+++ app/models/software_engineering_method.rb
@@ -2,8 +2,8 @@ class SoftwareEngineeringMethod < ActiveRecord::Base
   attr_accessible :description, :name
   has_and_belongs_to_many :software_engineering_methodologies
   has_and_belongs_to_many :evidence_items
-	
-	NON_RANSACKABLE_ATTRIBUTES = ['name']
+
+  NON_RANSACKABLE_ATTRIBUTES = ['id']
   def self.ransackable_attributes auth_object = nil
     (column_names - NON_RANSACKABLE_ATTRIBUTES) + _ransackers.keys
   end
