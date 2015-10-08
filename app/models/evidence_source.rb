8dd55aeff2585bff800c31e4082f4283be0552ca
Lukas Pirl <git@lukas-pirl.de>
2015-10-08 21:51:21 +1300

    added draft to search over multiple relations

--- app/models/evidence_source.rb
+++ app/models/evidence_source.rb
@@ -6,9 +6,8 @@ class EvidenceSource < AbstractUserCreatedModel
   has_many :research_metrics, dependent: :destroy
   has_and_belongs_to_many :research_methods
   has_and_belongs_to_many :participant_types
-	
-	NON_RANSACKABLE_ATTRIBUTES = []
 
+  NON_RANSACKABLE_ATTRIBUTES = []
   def self.ransackable_attributes auth_object = nil
     (column_names - NON_RANSACKABLE_ATTRIBUTES) + _ransackers.keys
   end
