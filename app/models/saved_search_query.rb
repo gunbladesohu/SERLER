86a7011aa1782efff8ebdef640d3d00d1e24b43e
Lukas Pirl <git@lukas-pirl.de>
2015-10-16 11:12:59 +1300

    made name for save search result/query unique

--- app/models/saved_search_query.rb
+++ app/models/saved_search_query.rb
@@ -1,4 +1,5 @@
 class SavedSearchQuery < ActiveRecord::Base
   attr_accessible :search_params, :comment, :name
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/saved_search_result.rb
+++ app/models/saved_search_result.rb
@@ -2,4 +2,5 @@ class SavedSearchResult < AbstractUserCreatedModel
   attr_accessible :comment, :name, :evidence_items, :evidence_item_ids
   has_and_belongs_to_many :evidence_items
   validates :name, :evidence_items, presence: true
+  validates :name, uniqueness: true
 end
